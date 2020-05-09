e8a94ad226a963c6c9330c8b09491112cb3545d0
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2020-05-09 11:38:49 -0700

    Create player.py

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+    cashMoney = 0
+    isDead = False
+    grain = 0
+    
+    
+    def __init__(self, playerId, diffyculty, gendyr):
+        self.playerId = playerId
+        self.diffyculty = diffyculty
+        self.gendyr = gendyr
+    
+    def turn():
+        pass
